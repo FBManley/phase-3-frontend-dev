2e41bc2e64147e61c531bdb4dcbe7e8deeb737b7
Jacob Manley <jbmanley45@gmail.com>
2022-10-24 16:56:07 -0700

    basic front end set

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+
+    // fetch("http://localhost:9292/test")
+    //     .then((r) => r.json())
+    //     .then((data) => console.log(data));
+
+    return (
+        <div>
+            <h1>My Home Page</h1>
+        </div>
+    )
+}
+export default Home
---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+           <NavLink to="/">Home</NavLink> 
+        // <NavLink to="/">Home</NavLink>
+    )
+}
+export default Navigation
